40b50cdf42933a6d46c877f53d3a62dd5155bbf5
jaeyeoneee <ljy8733@dgist.ac.kr>
2023-11-24 22:52:22 +0900

    feat: MemberRepository interface 추가

--- src/main/java/dgist/todocalendar/repository/MemberRepository.java
+++ src/main/java/dgist/todocalendar/repository/MemberRepository.java
@@ -1,5 +1,20 @@
 package dgist.todocalendar.repository;
 
+import dgist.todocalendar.domain.Member;
+import dgist.todocalendar.dto.MemberJoinDto;
+import dgist.todocalendar.dto.MemberUpdateDto;
+
+import java.util.Optional;
+
 public interface MemberRepository {
-    
+
+    Member save(MemberJoinDto memberJoinDto);
+    void delete(Long id);
+    void update(Long id, MemberUpdateDto memberUpdateDto);
+    //회원 정보 조회
+    Optional<Member> findById(Long id);
+    //로그인
+    Optional<Member> findByEmail(String email);
+    //회원가입 중복 확인
+    boolean existsByEmail(String email);
 }
