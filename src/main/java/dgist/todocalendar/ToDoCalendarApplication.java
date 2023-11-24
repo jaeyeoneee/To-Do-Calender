d10e10640c9b7f88c340b85e4efad0335b7333e3
jaeyeoneee <ljy8733@dgist.ac.kr>
2023-11-24 15:44:16 +0900

    first commit

---
+++ src/main/java/dgist/todocalendar/ToDoCalendarApplication.java
@@ -0,0 +1,13 @@
+package dgist.todocalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoCalendarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dgist/todocalendar/repository/MemberRepository.java
@@ -0,0 +1,5 @@
+package dgist.todocalendar.repository;
+
+public interface MemberRepository {
+    
+}
